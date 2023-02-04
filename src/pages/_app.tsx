aaf8c5096632c70d30e3a55efd1596ced1a26406
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2023-02-04 20:52:41 -0300

    feat: add redux toolkit as state management

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,18 @@
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { Provider } from "react-redux";
+import { store } from "redux/store";
 import "styles/globals.css";
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
     <>
       <Head>
-        <title><PROJECT_NAME></title>
+        <title>Binance App</title>
       </Head>
-      <Component {...pageProps} />
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
     </>
   );
 };
