1a4a5968dfa868bc9c9828662e359e54eb050ba8
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2023-04-03 19:30:13 -0300

    refactor: turn user store props into functions

--- src/types/user.d.ts
+++ src/types/user.d.ts
@@ -12,50 +12,55 @@ export type UserState = {
   /**
    * Clear user data from the store.
    */
-  clear: () => void;
+  clear(): void;
 
   /**
    * Deletes and signs out the user.
    */
-  delete: () => Promise<void>;
+  delete(): Promise<void>;
 
   /**
    * Signs in a user.
    */
-  login: (email: string, password: string) => Promise<void>;
+  login(email: string, password: string): Promise<void>;
 
   /**
    * Signs out a user.
    */
-  logout: () => Promise<void>;
+  logout(): Promise<void>;
 
   /**
    * Creates a new user account associated with the specified email address and password.
    */
-  register: (name: string, email: string, password: string) => Promise<void>;
+  register(name: string, email: string, password: string): Promise<void>;
 
   /**
    * Resets user password.
    */
-  resetPassword: (email: string) => Promise<void>;
+  resetPassword(email: string): Promise<void>;
 
   /**
    * Updates user email.
    */
-  updateEmail: (email: string) => Promise<void>;
+  updateEmail(email: string): Promise<void>;
 
   /**
    * Updates user name.
    */
-  updateName: (name: string) => Promise<void>;
+  updateName(name: string): Promise<void>;
 
   /**
    * Updates user password.
    */
-  updatePassword: (password: string) => Promise<void>;
+  updatePassword(password: string): Promise<void>;
 
   /**
    * Checks if the user is authenticated.
    */
-  verify: () => void;
+  verifyAuth(): void;
+
+  /**
+   * Sends a verification email to the user.
+   */
+  verifyEmail(): Promise<void>;
 };
