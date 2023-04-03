14a3d4ba90e4ef8f3e680faca98991ee9696d2b5
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2023-04-03 19:31:09 -0300

    feat: add ProtectedRoute to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
 import { Greetings } from "components/Greetings";
 import type { NextPage } from "next";
+import { ProtectedRoute } from "routes/ProtectedRoute";
 
 const Home: NextPage = (): JSX.Element => {
   return (
-    <div className="font-primary h-screen bg-zinc-900 flex items-center justify-center flex-col gap-2">
-      <Greetings />
-    </div>
+    <ProtectedRoute>
+      <div className="font-primary h-screen bg-zinc-900 flex items-center justify-center flex-col gap-2">
+        <Greetings />
+      </div>
+    </ProtectedRoute>
   );
 };
 
