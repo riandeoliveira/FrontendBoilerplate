e9d111420e282ff5951061b678a86e7d5b66f7cf
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2023-04-07 15:42:29 -0300

    refactor: update components and pages structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,6 @@
-import { Greetings } from "components/Greetings";
 import type { NextPage } from "next";
-import { ProtectedRoute } from "routes/ProtectedRoute";
+import Home from "./home";
 
-const Home: NextPage = (): JSX.Element => {
-  return (
-    <ProtectedRoute>
-      <div className="font-primary h-screen bg-zinc-900 flex items-center justify-center flex-col gap-2">
-        <Greetings />
-      </div>
-    </ProtectedRoute>
-  );
-};
+const Index: NextPage = (): JSX.Element => <Home />;
 
-export default Home;
+export default Index;
